5cf56d3ae0b59c03db62ceffa7b0a9c800eb9bf2
PatrickEBlack <G00410388@atu.ie>
2025-02-26 21:21:30 +0000

    Updated File Structure and UI

--- src/app/pages/manage-livestock/manage-livestock.page.ts
+++ src/app/pages/manage-livestock/manage-livestock.page.ts
@@ -1,20 +1,70 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
+import { 
+  IonContent, 
+  IonHeader, 
+  IonTitle, 
+  IonToolbar,
+  IonIcon
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { 
+  add, 
+  search, 
+  remove, 
+  eye, 
+  create, 
+  checkmarkCircle
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-manage-livestock',
   templateUrl: './manage-livestock.page.html',
   styleUrls: ['./manage-livestock.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent, 
+    IonHeader, 
+    IonTitle, 
+    IonToolbar,
+    IonIcon,
+    CommonModule, 
+    FormsModule,
+    RouterLink
+  ]
 })
 export class ManageLivestockPage implements OnInit {
+  livestockCount: number = 24;
+  pastureNumber: number = 2;
+  pastureSize: number = 4.6;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    // Register the Ionicons we need
+    addIcons({
+      'add': add,
+      'search': search,
+      'remove': remove,
+      'eye': eye,
+      'create': create,
+      'checkmark-circle': checkmarkCircle,
+    });
   }
 
+  ngOnInit() {
+    // Here you would fetch actual data from your service
+    this.fetchLivestockData();
+  }
+
+  fetchLivestockData() {
+    // This would be replaced with actual API calls to your backend
+    // For now, we're just using the hardcoded values
+    console.log('Fetching livestock data...');
+    
+    // Simulate API delay
+    setTimeout(() => {
+      console.log('Livestock data loaded');
+    }, 1000);
+  }
 }
