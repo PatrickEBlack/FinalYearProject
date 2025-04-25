cdd69636ef0d23c937e1a10febf55bc0b1243c7e
PatrickEBlack <G00410388@atu.ie>
2025-04-25 14:37:56 +0100

    Fix environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: '/api', 
-  openWeatherApiKey: '0ff1aa92a6ff5efafcc218d4f9a14f67'
+  openWeatherApiKey: '0ff1aa92a6ff5efafcc218d4f9a14f67',
+  weatherApiKey: '7a8e434a4a9b4200b64183519252702'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000/api', 
-  openWeatherApiKey: '0ff1aa92a6ff5efafcc218d4f9a14f67'
+  openWeatherApiKey: '0ff1aa92a6ff5efafcc218d4f9a14f67',
+  weatherApiKey: '7a8e434a4a9b4200b64183519252702'
 };
 
 /*
